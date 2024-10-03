87c58d1c3ba59ffa7df22642d34df410e7e2fb56
Alexandre LUX <alexandre.lux.92@gmail.com>
2024-10-03 10:03:00 +0200

    Ajoutez une gestion des fichiers utilisateur sur l'application

---
+++ middleware/multer-config.ts
@@ -0,0 +1,33 @@
+import multer from "multer";
+import { Request } from "express";
+
+const MIME_TYPES: { [key: string]: string } = {
+    "image/jpg": "jpg",
+    "image/jpeg": "jpg",
+    "image/png": "png",
+};
+
+// https://stackoverflow.com/questions/59097119/using-multer-diskstorage-with-typescript
+type DestinationCallback = (error: Error | null, destination: string) => void;
+type FileNameCallback = (error: Error | null, filename: string) => void;
+
+const storage = multer.diskStorage({
+    destination: (
+        req: Request,
+        file: Express.Multer.File,
+        callback: DestinationCallback
+    ) => {
+        callback(null, "images");
+    },
+    filename: (
+        req: Request,
+        file: Express.Multer.File,
+        callback: FileNameCallback
+    ) => {
+        const name = file.originalname.split(" ").join("_");
+        const extension = MIME_TYPES[file.mimetype];
+        callback(null, `${name}${Date.now()}.${extension}`);
+    },
+});
+
+export default multer({ storage }).single("image");
